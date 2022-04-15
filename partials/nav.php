f3f56747a0b89dab4314c0625e478d25c5773d48
paul080791 <78453577+paul080791@users.noreply.github.com>
2022-04-15 18:22:28 -0400

    adding list_item.php and edit_item_php

--- partials/nav.php
+++ partials/nav.php
@@ -62,7 +62,7 @@ session_start();
                             </a>
                             <ul class="dropdown-menu bg-secondary" aria-labelledby="rolesDropdown">
                                 <li><a class="dropdown-item" href="<?php echo get_url('admin/add_item.php'); ?>">Add Product</a></li>
-
+                                <li><a class="dropdown-item" href="<?php echo get_url('admin/list_item.php'); ?>">List Product</a></li>
                             </ul>
                         </li>
             <?php endif; ?>  
