3162ec1f5612b0f9d4fcfb054401442b1fadef64
paul080791 <78453577+paul080791@users.noreply.github.com>
2022-04-18 17:36:15 -0400

    Adding username next to logout button

--- partials/nav.php
+++ partials/nav.php
@@ -72,7 +72,7 @@ session_start();
                         </li>
             <?php endif; ?>  
             <?php if (is_logged_in()) : ?>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?php echo get_url('logout.php'); ?>">Logout <?php echo get_username() ?></a></li>
             <?php endif; ?>
         </ul>
     </div>
