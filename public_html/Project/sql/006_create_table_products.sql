1e385aee2a446d955f0291df9b35ead14d279eae
paul080791 <78453577+paul080791@users.noreply.github.com>
2022-04-18 14:12:00 -0400

    Adding functions on helpers, Create Shop page and fix columns from table

--- public_html/Project/sql/006_create_table_products.sql
+++ public_html/Project/sql/006_create_table_products.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS RM_Items(
     id int AUTO_INCREMENT PRIMARY  KEY,
-    name varchar(30) UNIQUE, -- alternatively you'd have a SKU that's unique
+    name varchar(40) UNIQUE, -- alternatively you'd have a SKU that's unique
     description text,
+    category text,
     stock int DEFAULT  0,
-    cost int DEFAULT  99999, -- my cost is int because I don't have regular currency; shop people may want to record it as pennies
+    cost DECIMAL(18,2) DEFAULT 0.00, -- my cost is int because I don't have regular currency; shop people may want to record it as pennies
     image text, -- this col type can't have a default value; this isn't required for any project, I chose to add it for mine
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
